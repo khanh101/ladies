122fdfe4a507af3794bf433042c340365ff275b5
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-20 14:20:32 +0800

    modified:   train.py

--- solution/train.py
+++ solution/train.py
@@ -2,21 +2,27 @@
 
 import argparse
 
+import torch
+import torch.nn as nn
+
+from module import GCN
+
+
+class Trainer(object):
+  def __init__(self, module: nn.Module):
+    pass
+  def train(x):
+    pass
+
+
+
 if __name__ == "__main__":
 
-  parser = argparse.ArgumentParser(description='Training GCN on Cora/CiteSeer/PubMed/Reddit Datasets')
-
-  '''
-      Dataset arguments
-  '''
-  parser.add_argument('--dataset', type=str, default='reddit',
-                      help='Dataset name: Cora/CiteSeer/PubMed/Reddit')
+  parser = argparse.ArgumentParser(description='Training GCN')
   parser.add_argument('--nhid', type=int, default=256,
-                      help='Hidden state dimension')
+                      help='Hidden layer dimension')
   parser.add_argument('--epoch_num', type=int, default= 100,
                       help='Number of Epoch')
-  parser.add_argument('--pool_num', type=int, default= 10,
-                      help='Number of Pool')
   parser.add_argument('--batch_num', type=int, default= 10,
                       help='Maximum Batch Number')
   parser.add_argument('--batch_size', type=int, default=512,
@@ -25,15 +31,14 @@ if __name__ == "__main__":
                       help='Number of GCN layers')
   parser.add_argument('--n_iters', type=int, default=2,
                       help='Number of iteration to run on a batch')
-  parser.add_argument('--n_stops', type=int, default=200,
-                      help='Stop after number of batches that f1 dont increase')
   parser.add_argument('--samp_num', type=int, default=64,
                       help='Number of sampled nodes per layer')
-  parser.add_argument('--sample_method', type=str, default='ladies',
-                      help='Sampled Algorithms: ladies/fastgcn/full')
+  parser.add_argument('--sample_method', type=str, default='default',
+                      help='Sampled Algorithms: default/ladies')
   parser.add_argument('--cuda', type=int, default=0,
                       help='Avaiable GPU ID')
 
-
-
   args = parser.parse_args()
+
+  gcn = GCN(in_features= 5, hidden_features= 5, out_features= 5, num_layers= 5, dropout= 0.5)
+
