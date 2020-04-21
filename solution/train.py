dd084eba97c3aa7b874010d32246cd6f087ec2b1
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-21 18:27:59 +0800

    modified:   train.py

--- solution/train.py
+++ solution/train.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
                       help='Number of GCN layers')
   parser.add_argument('--num_iterations', type=int, default=10,
                       help='Number of iteration to run on a batch')
-  parser.add_argument('--sampling_method', type=str, default='ladies',
+  parser.add_argument('--sampling_method', type=str, default='full',
                       help='Sampled Algorithms: full/ladies')
   parser.add_argument('--cuda', type=int, default=-1,
                       help='Avaiable GPU ID')
