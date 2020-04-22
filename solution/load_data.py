91d571920a04fc5f6c8f9b76df07b325010b8919
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-22 13:32:57 +0800

    modified:   load_data.py

--- solution/load_data.py
+++ solution/load_data.py
@@ -1,20 +1,8 @@
 import numpy as np
-import sys
 from types import SimpleNamespace
-import scipy.sparse as sp
-import pickle as pkl
-
-def parse_index_file(filename):
-    """Parse index file."""
-    index = []
-    for line in open(filename):
-        index.append(int(line.strip()))
-    return index
-
-def get_adj(edges, num_nodes):
-    adj = sp.coo_matrix((np.ones(edges.shape[0]), (edges[:, 0], edges[:, 1])),
-                    shape=(num_nodes, num_nodes), dtype=np.float32)
-    return adj + adj.T.multiply(adj.T > adj) - adj.multiply(adj.T > adj)
+from typing import List
+import networkx as nx
+import scipy.sparse as sparse
 
 def onehot_to_labels(onehot: np.ndarray) -> np.ndarray:
   out = []
@@ -30,53 +18,23 @@ def onehot_to_labels(onehot: np.ndarray) -> np.ndarray:
   return np.array(out)
   return np.array([np.where(row==1)[0][0] for row in onehot])
 
-def load():
-    names = ['x', 'y', 'tx', 'ty', 'allx', 'ally', 'graph']
-    objects = []
-    for i in range(len(names)):
-        with open("./dataset/ind.{}.{}".format('citeseer', names[i]), 'rb') as f:
-            if sys.version_info > (3, 0):
-                objects.append(pkl.load(f, encoding='latin1'))
-            else:
-                objects.append(pkl.load(f))
+def load(cluster_size: List[int] = [100, 100], prob_matrix: List[List[float]] = [[0.1, 0.001],[0.001, 0.1]]):
+    net = nx.generators.community.stochastic_block_model(cluster_size, prob_matrix)
+    adj_matrix = nx.adjacency_matrix(net)
+    num_nodes = sum(cluster_size)
+    idx = np.arange(num_nodes)
+    np.random.shuffle(idx)
+    idx_train = idx[:int(0.8 * len(idx))]
+    idx_valid = idx[int(0.8 * len(idx)): int(0.9 * len(idx))]
+    idx_test = idx[int(0.9 * len(idx)):]
 
-    x, y, tx, ty, allx, ally, graph = tuple(objects)
-    test_idx_reorder = parse_index_file("dataset/ind.{}.test.index".format('citeseer'))
-    test_idx_range = np.sort(test_idx_reorder)
-
-    test_idx_range_full = range(min(test_idx_reorder), max(test_idx_reorder)+1)
-    tx_extended = sp.lil_matrix((len(test_idx_range_full), x.shape[1]))
-    tx_extended[test_idx_range-min(test_idx_range), :] = tx
-    tx = tx_extended
-    ty_extended = np.zeros((len(test_idx_range_full), y.shape[1]))
-    ty_extended[test_idx_range-min(test_idx_range), :] = ty
-    ty = ty_extended
-
-    features = sp.vstack((allx, tx)).tolil()
-    features[test_idx_reorder, :] = features[test_idx_range, :]
-
-    labels = np.vstack((ally, ty))
-    labels[test_idx_reorder, :] = labels[test_idx_range, :]
-
-    idx_test = np.array(test_idx_range.tolist())
-    idx_train = np.array(range(len(y)))
-    idx_val = np.array(range(len(y), len(y)+500))
-
-
-    edges = []
-    for s in graph:
-        for t in graph[s]:
-            edges += [[s, t]]
-
-    adj_matrix = get_adj(np.array(edges), labels.shape[0])
-    idx_train, idx_test = idx_test, idx_train
-
-    labels = onehot_to_labels(labels)
+    labels = np.array(list(map(lambda node: net.nodes[node]["block"], net.nodes)))
+    features = sparse.csr_matrix(np.zeros(shape = (num_nodes, 1)))
 
     data = SimpleNamespace(
         adj_matrix= adj_matrix,
         train_nodes= idx_train,
-        valid_nodes= idx_val,
+        valid_nodes= idx_valid,
         test_nodes=idx_test,
         labels= labels,
         features= features,
