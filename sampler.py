2bb4c63bd6d5ab8a335ddbc8672b6f356f1d71b4
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-23 14:50:32 +0800

    modified:   sampler.py

--- sampler.py
+++ sampler.py
@@ -7,18 +7,23 @@ from types import SimpleNamespace
 from typing import Tuple, List
 from utils import row_normalize, sparse_fill
 
-"""
-def full_sampler(seed: int, batch_nodes: np.ndarray, samp_num_list: nd.ndarray, num_nodes: int, lap2_matrix: sp.sparse.spmatrix, num_layers: int) -> Tuple[List[torch.Tensor], np.ndarray, np.ndarray]:
-    seed: seed
-    batch_nodes: 1d array of nodes at output layer
-    samp_num_list: number of sampled node in each layer
-    num_nodes: number of nodes
-    lap_matrix: row-normalized laplacian matrix
-    lap2_matrix: square row-normalized laplacian matrix
-    num_layers: number of layers
-"""
+def sampler(batch_nodes: np.ndarray, samp_num_list: np.ndarray, num_nodes: int, lap_matrix: sparse.csr_matrix, lap2_matrix: sparse.csr_matrix, num_layers: int) -> SimpleNamespace:
+    ##### INPUT ######################################################
+    # batch_nodes : sampled nodes at output
+    # samp_num_list: array of number of sampled nodes at all layers
+    # num_nodes : number of graph nodes
+    # lap_matrix : row-normalized laplacian matrix
+    # lap2_matrix : squared lap_matrix (precomputed)
+    # num_layers : len(samp_num_list)
+    ##### OUTPUT ######################################################
+    # adjs : P matrix
+    # input_nodes: sampled nodes at input
+    # output_nodes : batch_nodes
+    ###################################################################
+    raise Exception("sampler must not be called")
 
 def full_sampler(batch_nodes: np.ndarray, samp_num_list: np.ndarray, num_nodes: int, lap_matrix: sparse.csr_matrix, lap2_matrix: sparse.csr_matrix, num_layers: int) -> SimpleNamespace:
+    # simply sample the full lap_matrix for every layers
     sample = SimpleNamespace(
         adjs= [lap_matrix for _ in range(num_layers)],
         input_nodes= np.arange(num_nodes),
@@ -37,30 +42,29 @@ def ladies_sampler(batch_nodes: np.ndarray, samp_num_list: np.ndarray, num_nodes
         Sample nodes from top to bottom, based on the probability computed adaptively (layer-dependent).
     '''
     for d in range(num_layers):
-        #     row-select the lap2_matrix (U2) by previously sampled nodes
-        U2 = lap2_matrix[previous_nodes , :] # square normalized laplacian matrix
-        #     Only use the upper layer's neighborhood to calculate the probability.
+        # row-select the lap2_matrix (U2) by previously sampled nodes
+        U2 = lap2_matrix[previous_nodes , :]
+        # calculate sampling probablities
         pi = np.sum(U2, axis=0)
         p = pi / np.sum(pi)
+        # get number of sampled nodes
         s_num = np.min([np.sum(p > 0), samp_num_list[d]])
         p = p.view(np.ndarray).flatten()
-        #     sample the next layer's nodes based on the adaptively probability (p).
+        # sample the next layer's nodes based on the adaptively probability (p).
         after_nodes = np.random.choice(num_nodes, s_num, p = p, replace = False)
-        #     Add output nodes for self-loop
+        # Add output nodes for self-loop
         after_nodes = np.unique(np.concatenate((after_nodes, batch_nodes)))
-        #     row-select and col-select the lap_matrix (U), and then devided by the sampled probability for 
-        #     unbiased-sampling. Finally, conduct row-normalization to avoid value explosion.      
-
+        # row-select and col-select the lap_matrix (U), and then devided by the sampled probability for unbiased-sampling.
         adj = lap_matrix[previous_nodes, :][:, after_nodes]
         adj = adj.multiply(1/ p[after_nodes])
+        # conduct row-normalization to avoid value explosion.      
         adj = row_normalize(adj)
-
+        # fill the sub-matrix into the original
         adj = sparse_fill(lap_matrix.shape, adj, previous_nodes, after_nodes)
-
         adjs.append(adj)
-        #     Turn the sampled nodes as previous_nodes, recursively conduct sampling.
+        # turn the sampled nodes as previous_nodes, recursively conduct sampling.
         previous_nodes = after_nodes
-    #     Reverse the sampled probability from bottom to top. Only require input how the lastly sampled nodes.
+    # Reverse the sampled probability from bottom to top. Only require input how the lastly sampled nodes.
     adjs.reverse()
 
     sample = SimpleNamespace(
