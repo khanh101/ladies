5323193b86e5254ebac25a3f0852c57ff3f78be0
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-28 12:45:52 +0800

    modified:   load_data.py

--- load_data.py
+++ load_data.py
@@ -9,11 +9,9 @@ def load_random_block(cluster_size: List[int] = [5, 5], prob_matrix: List[List[f
     net = nx.generators.community.stochastic_block_model(cluster_size, prob_matrix)
     adj_matrix = nx.adjacency_matrix(net)
     num_nodes = sum(cluster_size)
-    idx = np.arange(num_nodes)
-    np.random.shuffle(idx)
+    idx = np.random.permutation(num_nodes)
     idx_train = idx[:int(0.8 * len(idx))]
-    idx_valid = idx[int(0.8 * len(idx)): int(0.9 * len(idx))]
-    idx_test = idx[int(0.9 * len(idx)):]
+    idx_valid = idx[int(0.8 * len(idx)):]
 
     labels = np.array(list(map(lambda node: net.nodes[node]["block"], net.nodes)))
 
@@ -27,7 +25,6 @@ def load_random_block(cluster_size: List[int] = [5, 5], prob_matrix: List[List[f
         adj_matrix= adj_matrix,
         train_nodes= idx_train,
         valid_nodes= idx_valid,
-        test_nodes=idx_test,
         labels= labels,
         features= features,
     ) 
