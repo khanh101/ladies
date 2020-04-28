3ee8886b6fe77f5fd64b218963d4e71543df5663
Nguyen Ngoc Khanh <nguyenngockhanh.pbc@gmail.com>
2020-04-28 12:55:06 +0800

    acscavasc

--- sampler.py
+++ sampler.py
@@ -8,70 +8,70 @@ from typing import Tuple, List
 from utils import row_normalize, sparse_fill
 
 def sampler(batch_nodes: np.ndarray, samp_num_list: np.ndarray, num_nodes: int, lap_matrix: sparse.csr_matrix, lap2_matrix: sparse.csr_matrix, num_layers: int) -> SimpleNamespace:
-    ##### INPUT ######################################################
-    # batch_nodes : sampled nodes at output
-    # samp_num_list: array of number of sampled nodes at all layers
-    # num_nodes : number of graph nodes
-    # lap_matrix : row-normalized laplacian matrix
-    # lap2_matrix : squared lap_matrix (precomputed)
-    # num_layers : len(samp_num_list)
-    ##### OUTPUT ######################################################
-    # adjs : P matrix
-    # input_nodes: sampled nodes at input
-    # output_nodes : batch_nodes
-    ###################################################################
-    raise Exception("sampler must not be called")
+  ##### INPUT ######################################################
+  # batch_nodes : sampled nodes at output
+  # samp_num_list: array of number of sampled nodes at all layers
+  # num_nodes : number of graph nodes
+  # lap_matrix : row-normalized laplacian matrix
+  # lap2_matrix : squared lap_matrix (precomputed)
+  # num_layers : len(samp_num_list)
+  ##### OUTPUT ######################################################
+  # adjs : P matrix
+  # input_nodes: sampled nodes at input
+  # output_nodes : batch_nodes
+  ###################################################################
+  raise Exception("sampler must not be called")
 
 def full_sampler(batch_nodes: np.ndarray, samp_num_list: np.ndarray, num_nodes: int, lap_matrix: sparse.csr_matrix, lap2_matrix: sparse.csr_matrix, num_layers: int) -> SimpleNamespace:
-    # simply sample the full lap_matrix for every layers
-    sample = SimpleNamespace(
-        adjs= [lap_matrix for _ in range(num_layers)],
-        input_nodes= np.arange(num_nodes),
-        output_nodes= batch_nodes,
-    )
-    return sample
+  # simply sample the full lap_matrix for every layers
+  sample = SimpleNamespace(
+    adjs= [lap_matrix for _ in range(num_layers)],
+    input_nodes= np.arange(num_nodes),
+    output_nodes= batch_nodes,
+  )
+  return sample
 
 def ladies_sampler(batch_nodes: np.ndarray, samp_num_list: np.ndarray, num_nodes: int, lap_matrix: sparse.csr_matrix, lap2_matrix: sparse.csr_matrix, num_layers: int) -> SimpleNamespace:
-    '''
-        LADIES_Sampler: Sample a fixed number of nodes per layer. The sampling probability (importance)
-                         is computed adaptively according to the nodes sampled in the upper layer.
-    '''
-    previous_nodes = batch_nodes
-    adjs  = []
-    '''
-        Sample nodes from top to bottom, based on the probability computed adaptively (layer-dependent).
-    '''
-    for d in range(num_layers):
-        # row-select the lap2_matrix (U2) by previously sampled nodes
-        U2 = lap2_matrix[previous_nodes , :]
-        # calculate sampling probablities
-        pi = np.sum(U2, axis=0)
-        p = pi / np.sum(pi)
-        # get number of sampled nodes
-        s_num = np.min([np.sum(p > 0), samp_num_list[d]])
-        p = p.view(np.ndarray).flatten()
-        # sample the next layer's nodes based on the adaptively probability (p).
-        after_nodes = np.random.choice(num_nodes, s_num, p = p, replace = False)
-        # Add output nodes for self-loop
-        after_nodes = np.unique(np.concatenate((after_nodes, batch_nodes)))
-        # row-select and col-select the lap_matrix (U), and then devided by the sampled probability for unbiased-sampling.
-        adj = lap_matrix[previous_nodes, :][:, after_nodes]
-        adj = adj.multiply(1/ p[after_nodes])
-        # conduct row-normalization to avoid value explosion.      
-        adj = row_normalize(adj)
-        # fill the sub-matrix into the original
-        adj = sparse_fill(lap_matrix.shape, adj, previous_nodes, after_nodes)
-        adjs.append(adj)
-        # turn the sampled nodes as previous_nodes, recursively conduct sampling.
-        previous_nodes = after_nodes
-    # Reverse the sampled probability from bottom to top. Only require input how the lastly sampled nodes.
-    adjs.reverse()
+  '''
+    LADIES_Sampler: Sample a fixed number of nodes per layer. The sampling probability (importance)
+             is computed adaptively according to the nodes sampled in the upper layer.
+  '''
+  previous_nodes = batch_nodes
+  adjs  = []
+  '''
+    Sample nodes from top to bottom, based on the probability computed adaptively (layer-dependent).
+  '''
+  for d in range(num_layers):
+    # row-select the lap2_matrix (U2) by previously sampled nodes
+    U2 = lap2_matrix[previous_nodes , :]
+    # calculate sampling probablities
+    pi = np.sum(U2, axis=0)
+    p = pi / np.sum(pi)
+    # get number of sampled nodes
+    s_num = np.min([np.sum(p > 0), samp_num_list[d]])
+    p = p.view(np.ndarray).flatten()
+    # sample the next layer's nodes based on the adaptively probability (p).
+    after_nodes = np.random.choice(num_nodes, s_num, p = p, replace = False)
+    # Add output nodes for self-loop
+    after_nodes = np.unique(np.concatenate((after_nodes, batch_nodes)))
+    # row-select and col-select the lap_matrix (U), and then devided by the sampled probability for unbiased-sampling.
+    adj = lap_matrix[previous_nodes, :][:, after_nodes]
+    adj = adj.multiply(1/ p[after_nodes])
+    # conduct row-normalization to avoid value explosion.    
+    adj = row_normalize(adj)
+    # fill the sub-matrix into the original
+    adj = sparse_fill(lap_matrix.shape, adj, previous_nodes, after_nodes)
+    adjs.append(adj)
+    # turn the sampled nodes as previous_nodes, recursively conduct sampling.
+    previous_nodes = after_nodes
+  # Reverse the sampled probability from bottom to top. Only require input how the lastly sampled nodes.
+  adjs.reverse()
 
-    sample = SimpleNamespace(
-        adjs= adjs,
-        input_nodes= previous_nodes,
-        output_nodes= batch_nodes,
-    )
-    return sample
+  sample = SimpleNamespace(
+    adjs= adjs,
+    input_nodes= previous_nodes,
+    output_nodes= batch_nodes,
+  )
+  return sample
 if __name__ == "__main__":
   pass
--- utils.py
+++ utils.py
@@ -9,38 +9,38 @@ import sys
 import pickle as pkl
 
 def adj_to_lap_matrix(adj_matrix: sparse.csr_matrix) -> sparse.csr_matrix:
-    lap_matrix = adj_matrix + sparse.eye(adj_matrix.shape[0])
-    return lap_matrix
+  lap_matrix = adj_matrix + sparse.eye(adj_matrix.shape[0])
+  return lap_matrix
 
 def sparse_fill(shape: np.ndarray, mx: sparse.csr_matrix, row: np.ndarray = None, col: np.ndarray= None) -> sparse.csr_matrix:
-    if row is None:
-        row = np.arange(shape[0])
-    if col is None:
-        col = np.arange(shape[1])
-    lil = sparse.lil_matrix(shape)
-    mx = mx.toarray()
-    for r, rr in enumerate(row):
-        for c, cc in enumerate(col):
-            lil[rr, cc] = mx[r, c]
-    return sparse.csr_matrix(lil)
+  if row is None:
+    row = np.arange(shape[0])
+  if col is None:
+    col = np.arange(shape[1])
+  lil = sparse.lil_matrix(shape)
+  mx = mx.toarray()
+  for r, rr in enumerate(row):
+    for c, cc in enumerate(col):
+      lil[rr, cc] = mx[r, c]
+  return sparse.csr_matrix(lil)
 
 def row_normalize(mx):
-    rowsum = np.array(mx.sum(1))
-    rowsum[rowsum == 0] = 1 # rowsum == 0 -> no need to divide
-    r_inv = np.power(rowsum, -1).flatten()
-    r_inv[np.isinf(r_inv)] = 0.
-    r_mat_inv = sparse.diags(r_inv)
+  rowsum = np.array(mx.sum(1))
+  rowsum[rowsum == 0] = 1 # rowsum == 0 -> no need to divide
+  r_inv = np.power(rowsum, -1).flatten()
+  r_inv[np.isinf(r_inv)] = 0.
+  r_mat_inv = sparse.diags(r_inv)
 
-    mx = r_mat_inv.dot(mx)
-    return mx
+  mx = r_mat_inv.dot(mx)
+  return mx
 
 def sparse_mx_to_torch_sparse_tensor(sparse_mx: sparse.csr_matrix) -> torch.FloatTensor:
-    """Convert a scipy sparse matrix to a torch sparse tensor."""
-    sparse_mx = sparse_mx.tocoo().astype(np.float32)
-    if len(sparse_mx.row) == 0 and len(sparse_mx.col) == 0:
-        indices = torch.LongTensor([[], []])
-    else:
-        indices = torch.from_numpy(np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
-    values = torch.from_numpy(sparse_mx.data)
-    shape = torch.Size(sparse_mx.shape)
-    return torch.sparse.FloatTensor(indices, values, shape)
+  """Convert a scipy sparse matrix to a torch sparse tensor."""
+  sparse_mx = sparse_mx.tocoo().astype(np.float32)
+  if len(sparse_mx.row) == 0 and len(sparse_mx.col) == 0:
+    indices = torch.LongTensor([[], []])
+  else:
+    indices = torch.from_numpy(np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
+  values = torch.from_numpy(sparse_mx.data)
+  shape = torch.Size(sparse_mx.shape)
+  return torch.sparse.FloatTensor(indices, values, shape)
